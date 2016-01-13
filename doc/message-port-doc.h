d669543231d67b7ebb5e5ff6411f9875757655c5
SukHyung, Kang <shine.kang@samsung.com>
2016-01-14 08:36:30 +0900

    Add doc files for API references
    Change-Id: Ib554440be2dcd33a71dba77b760791a231491c46
    Signed-off-by: SukHyung, Kang <shine.kang@samsung.com>

---
+++ doc/message-port-doc.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __TIZEN_APPFW_MESSAGE_PORT_DOC_H__
+#define __TIZEN_APPFW_MESSAGE_PORT_DOC_H__
+
+/**
+ * @ingroup CAPI_APPLICATION_FRAMEWORK
+ * @defgroup CAPI_MESSAGE_PORT_MODULE Message Port
+ * @brief The @ref CAPI_MESSAGE_PORT_MODULE API provides functions to send and receive messages between applications.
+ *
+ * @section CAPI_MESSAGE_PORT_MODULE_HEADER Required Header
+ *   \#include <message_port.h>
+ *
+ * @section CAPI_MESSAGE_PORT_MODULE_OVERVIEW Overview
+ * The @ref CAPI_MESSAGE_PORT_MODULE API provides functions for passing messages between applications.
+ * An application should register its own local port to receive messages from remote applications.
+ * If a remote application sends a message, the registered callback function of the local port is called. @n
+ * The trusted message-port API allows communications between applications that are signed by the same developer (author) certificate. 
+*/
+
+#endif /* __TIZEN_APPFW_MESSAGE_PORT_DOC_H__ */
